25588e8ba0f1c2fafec6499f8d2fcabddb47aa9f
RonishGhouri <ronishemanual974@gmail.com>
2025-04-07 19:54:28 +0500

    last commit

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,14 +4,10 @@ const Dropdown = () => {
   return (
     <div className="w-full ">
       {/* The select dropdown */}
-      <select className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:border-black">
-        <option value="" disabled>
-          When (Date)
-        </option>
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </select>
+      <input
+        type="date"
+        className="border-2 border-black/20 w-full h-10 rounded-md px-2"
+      />
     </div>
   );
 };
